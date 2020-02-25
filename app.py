a1296ba069e3a37e730c0c6e32cede954f98b0a8
潘健博 <panjianbo@basecity.com>
2020-02-25 15:22:18 +0800

    修改格式

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask
 from pip._vendor import requests
+import json
+import time
 
 app = Flask(__name__)
 
@@ -12,20 +14,25 @@ def get_account_hashrate():
     args = {'coin': 'BTC'}
     headers = {'X-API-KEY': 'ed91396c2c558a299ed41ea719a75ba0'}
     r = requests.get('https://pool.viabtc.com/res/openapi/v1/hashrate', params=args, headers=headers)
-    return r.text
+    j = json.loads(r.text)
+    out = ''
+    out += '当前账号余额：' + j["balance"][0]['amount'] + '\n'
+    return out
 
 def get_account_wallet():
     headers = {'X-API-KEY': 'ed91396c2c558a299ed41ea719a75ba0'}
     r = requests.get('https://www.viabtc.com/res/openapi/v1/account', headers=headers)
-    return r.text
+    j = json.loads(r.text)
+    out = ''
+    out += '活跃机器：'+j["active_workers"]+'\n'
+    out += '一小时算力：'+str(float(j["hashrate_1hour"])/1000000000000)+'\n'
+    return out
 
 @app.route("/")
 def hello():
     responses = ''
     responses += get_account_hashrate()
-    responses += '\n'
     responses += get_account_wallet()
-    import time
     to_send_msg = 'https://sc.ftqq.com/SCU61940T3553c7eb29d0115392096e47a7f60fd35e54a4c36cd76.send?text='+time.asctime(time.localtime(time.time()))+'&desp='+responses
     requests.get(to_send_msg)
     return responses
