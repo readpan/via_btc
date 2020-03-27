5ec098f84607fb805dcfc3d861034589b3395cb7
潘健博 <panjianbo@basecity.com>
2020-03-27 11:09:41 +0800

    新增: 新增获得访问用户ip

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 # app.py
 # -*- coding: UTF-8 -*-
 from flask import Flask
+from flask import request
 from pip._vendor import requests
 import json
 import time
 
 app = Flask(__name__)
 
+
+def get_request_ip():
+    try:
+        ip = request.remote_addr
+        return '当前访问ip: ' + ip + '<br>'
+    except Exception as e:
+        return 'unknown ip address <br>'
 
 
 def get_account_hashrate():
@@ -22,6 +30,7 @@ def get_account_hashrate():
     out += '一小时算力：' + str(float(j["hashrate_1hour"]) / 1000000000000) + '<br>'
     return out
 
+
 def get_account_wallet():
     headers = {'X-API-KEY': 'ed91396c2c558a299ed41ea719a75ba0'}
     r = requests.get('https://www.viabtc.com/res/openapi/v1/account', headers=headers)
@@ -32,14 +41,18 @@ def get_account_wallet():
     out += '当前账号余额：' + str(j['balance'][0]['amount']) + '<br>'
     return out
 
+
 @app.route("/")
 def hello():
     responses = ''
     responses += get_account_hashrate()
     responses += get_account_wallet()
-    to_send_msg = 'https://sc.ftqq.com/SCU61940T3553c7eb29d0115392096e47a7f60fd35e54a4c36cd76.send?text='+time.asctime(time.localtime(time.time()))+'&desp='+responses
+    responses += get_request_ip()
+    to_send_msg = 'https://sc.ftqq.com/SCU61940T3553c7eb29d0115392096e47a7f60fd35e54a4c36cd76.send?text=' + time.asctime(
+        time.localtime(time.time())) + '&desp=' + responses
     requests.get(to_send_msg)
     return responses
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
